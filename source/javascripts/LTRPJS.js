ae9f9643ca8d167d2ffd840fc56dcb92a15abbac
MattySheikh <malsheikh@findthebest.com>
2014-03-06 10:01:07 -0800

    FTB: Add Script for LT Research Projects

---
+++ source/javascripts/LTRPJS.js
@@ -0,0 +1,22 @@
+year = '';
+
+columns_we_want = [1, 8, 11, 12, 21, 22, 23, 24];
+count = 0;
+data = [];
+headers = [];
+$('#rushing_and_receiving').find('tr').not('tr.over_header').find('th').each(function(){
+	if ($.inArray(count, columns_we_want) > -1) {
+		headers.push($(this).text().replace(/\D/g,''));
+	}
+	count++;
+});
+
+count = 0;
+$('#div_rushing_and_receiving').find('tbody').find('[id*='+year+'] td').each(function(){
+	if ($.inArray(count, columns_we_want) > -1) {
+		data.push($(this).text());
+	}
+	count++;
+})
+
+console.log(data.join('\t'));
